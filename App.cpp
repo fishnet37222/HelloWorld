64182febff915ca2c5fea26ed786118d13fcf958
David Frischknecht <fishnet37222@gmail.com>
2021-03-05 12:58:20 -0500

    first commit

---
+++ App.cpp
@@ -0,0 +1,12 @@
+#include "App.h"
+#include "MainWindow.h"
+
+wxIMPLEMENT_APP(App);
+
+bool App::OnInit()
+{
+	auto* win = new MainWindow();
+	this->SetTopWindow(win);
+	win->Show();
+	return true;
+}
---
+++ MainWindow.cpp
@@ -0,0 +1,12 @@
+#include "MainWindow.h"
+
+MainWindow::MainWindow() : wxFrame(nullptr, wxID_ANY, "Hello World")
+{
+	this->InitUI();
+}
+
+void MainWindow::InitUI()
+{
+	this->SetBackgroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_FRAMEBK));
+	this->CenterOnScreen();
+}
